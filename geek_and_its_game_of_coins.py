afb5fd47fe4df03539720814ca3842bf4ca23e3b
Mohamed Ameer Noufil N <ameer03062003@gmail.com>
2024-05-29 08:36:58 +0000

    potd

---
+++ geek_and_its_game_of_coins.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findWinner(self, n : int, x : int, y : int) -> int:
+        dp = [False] * (n+1)
+        for i in range(1,len(dp)):
+            os = i-1
+            xs = i-x
+            ys = i-y
+            ans = dp[os]
+            if(xs>=0):
+                ans = ans and dp[xs]
+            if(ys>=0):
+                ans = ans and dp[ys]
+            dp[i] = not ans
+        return int(dp[-1])
