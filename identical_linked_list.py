7bcbcc4db2a8c6edac78773eb436db07febaadd6
Mohamed Ameer Noufil N <ameer03062003@gmail.com>
2024-06-29 09:56:22 +0530

    gfg-potd

---
+++ identical_linked_list.py
@@ -0,0 +1,10 @@
+def areIdentical(head1, head2):
+    while head1 and head2:
+        if head1.data == head2.data:
+            head1 = head1.next
+            head2 = head2.next
+        else:
+            return False
+    if head1 is None and head2 is None:
+        return True
+    return False
