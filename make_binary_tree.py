74d5d0b1360701ced1bd11c266495a865398b7d7
Mohamed Ameer Noufil N <ameer03062003@gmail.com>
2024-07-01 17:31:16 +0530

    gfg-potd

---
+++ make_binary_tree.py
@@ -0,0 +1,23 @@
+def convert(head):
+    treeNode = Tree(head.data)
+    head = head.next
+    stack = deque([treeNode])
+    rootNode = treeNode
+    while len(stack) and head:
+        
+        treeNode = stack.popleft()
+        
+        first = head.data
+        treeNode.left = Tree(first)
+        stack.append(treeNode.left)
+        
+        if head.next:
+            second = head.next.data
+            treeNode.right = Tree(second)
+            stack.append(treeNode.right)
+        
+            head = head.next.next
+        else:
+            head = head.next
+        
+    return rootNode
